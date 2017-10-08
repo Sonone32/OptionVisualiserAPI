2143408c77c067ec2e491f9eb63d2b72402aa083
sonone32 <sonone32@ohsweetcherry.com>
2017-10-08 04:18:40 +0000

    set up logging

--- __main__.py
+++ __main__.py
@@ -4,6 +4,28 @@ from interest_rate import interest_rate
 from sanic import Sanic
 from sanic_cors import CORS
 from yahoo_finance_chain import yahoo_finance
+from sanic.config import LOGGING as L
+
+# LOGGING can be found in https://github.com/channelcat/sanic/blob/master/sanic/log.py
+A = {'class': 'logging.handlers.RotatingFileHandler',
+        'filters': ['accessFilter'],
+        'formatter': 'access',
+        'filename': 'access.log',
+        'maxBytes': 1024 * 1024 * 10,
+        'backupCount': 5,
+        }
+
+
+E = {'class': 'logging.handlers.RotatingFileHandler',
+        'filters': ['errorFilter'],
+        'formatter': 'simple',
+        'filename': 'error.log',
+        'maxBytes': 1024 * 1024 * 10,
+        'backupCount': 5,
+        }
+
+L['handlers']['accessStream'] = A
+L['handlers']['errorStream'] = E
 
 app = Sanic(__name__)
 CORS(app)
@@ -22,4 +44,4 @@ async def tear_down_server(app, loop):
     app.redis.close()
     print('Server shutdown')
 
-app.run(host="0.0.0.0", port=8000, debug=False)
+app.run(host="0.0.0.0", port=8000, debug=False, log_config=L)
